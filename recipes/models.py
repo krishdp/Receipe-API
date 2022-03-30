1622aaa02bbcc71e0e1d228d3881d6ae141ef291
Krishnandu Ranjan Roy <roykrish9899@gmail.com>
2022-03-30 08:53:08 +0530

    Adding API.

---
+++ recipes/models.py
@@ -0,0 +1,53 @@
+from venv import create
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+class Recipe(models.Model):
+    DIET_CHOICES = [
+        ('balanced', 'balanced'),
+        ('high-protin', 'high-protin'),
+        ('high-fibre', 'high-fibre'),
+        ('low-fat', 'low-fat'),
+        ('low-crab', 'low-crab'),
+        ('low-sodium', 'low-sodium')
+    ]
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    time_mins = models.PositiveBigIntegerField()
+    ingredients = models.ManyToManyField('Ingredient')
+    diet = models.CharField(max_length=15, choices=DIET_CHOICES, default='balanced')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    objects = models.Manager()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    calories = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    objects = models.Manager()
+
+
+class Upvote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    objects = models.Manager()
+
+
+@receiver(post_save,sender=User)
+def create_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
---
+++ recipes/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from .models import Recipe, Ingredient, Upvote
+from .serializers import RecipeSerializer, IngredientSerializer, UpvoteSerializer, RecipeDetailserializer, UserRegistrationSerializer
+
+
+class RecipeCreateView(generics.CreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class RecipeListView(generics.ListAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class IngredientCreateView(generics.ListCreateAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class CreateUpvoteView(generics.ListCreateAPIView):
+    serializer_class = UpvoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        recipe = Recipe.objects.get(pk=self.kwargs['pk'])
+        return Upvote.objects.filter(user=user, recipe=recipe)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError("You've already voted on this.")
+        user = self.request.user
+        recipe = Recipe.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=user, recipe=recipe)
+
+
+class RecipeDetailsView(generics.RetrieveAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeDetailserializer
+    permission_classes = [permissions.AllowAny]
+
+
+class RecipeUpdateView(generics.RetrieveDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        recipe = Recipe.objects.filter(author=self.request.user, pk=kwargs['pk'])
+        if recipe.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError("Hry There..!! This is'ny your recipe..")
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.get(user=user).key
+            data = {
+                'token': token
+            }
+        else:
+            data = serializer.errors
+        return Response(data=data, status=201)
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = AuthTokenSerializer
+    
+    def create(self, request):
+        return ObtainAuthToken().as_view()(request=request._request)
